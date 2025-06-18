fe5ff148e2df06fec538316fa248a7d60be924b9
zdgterhgfcwerw <mnms1124@gmail.com>
2025-06-18 09:53:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -7,3 +9,8 @@ async def send_email(request: Request):
     data = await request.json()
     print("받은 이메일 데이터:", data)
     return {"status": "ok"}
+
+# 실행 코드 추가
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render가 요구하는 포트 사용
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
